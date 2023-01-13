c75f390771ad2f5946056ee88537e80912ce34a1
Harumi Yamashita <bz1786354@gmail.com>
2023-01-13 16:00:16 -0500

    add header CROS

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-      return [
-        {
-          source: '/:path*',
-
-          destination: 'https://api.revery.ai/:path*',
-        },
-      ]
-    },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "https://api.revery.ai/console/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
